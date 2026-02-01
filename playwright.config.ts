f8d8dcc715eb2add1bbb652fe13c72c4ff088d6d
Polina Didyk <polina.didyk@tech-stack.io>
2026-02-01 15:44:47 +0100

    commit changes

--- playwright.config.ts
+++ playwright.config.ts
@@ -22,9 +22,12 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: 'html', 'list', 'json', { outputFile: 'test-results.json' },
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
+    screenshot: 'only-on-failure',
+    video: 'retain-on-failure',
+    trace: 'retain-on-failure'
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
